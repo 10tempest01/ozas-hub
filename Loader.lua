f0bb1b24551fc50e91a00bbf993bfa8f93173cff
Tempest <59133811+10tempest01@users.noreply.github.com>
2025-06-16 20:34:37 +0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,15 @@
-print("test")
+if not script_key and not script_id and not settings then return warn("something is missing buddy") end
+
+settings = getgenv().settings
+
+local succ, err = pcall(function()
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/%s.lua"):format(script_id))()
+end)
+
+if err then
+    warn(("Ozas Hub failed to load.\nError:%s"):format(tostring(err)))
+end
+
+local qot = queue_on_teleport and queue_on_teleport or function(...) return (...) end
+
+qot([[loadstring(game:HttpGet("https://raw.githubusercontent.com/10tempest01/ozas-hub/refs/heads/main/Loader.lua"))()]])
