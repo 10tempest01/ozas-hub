d353f0d8f32b4fdb2069cfb9d0b6f89f98ff0136
Tempest <59133811+10tempest01@users.noreply.github.com>
2025-06-16 20:57:00 +0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,8 +1,8 @@
 if not script_key and not script_id and not settings then return game.Players.LocalPlayer:Kick("Failed to load. You might be using an old loader, please get the new one in the #script channel.") end
 
-script_key = getgenv().script_key
-script_id = getgenv().script_id
-ozas_hub_settings = getgenv().ozas_hub_settings
+getgenv().script_key = script_key
+getgenv().script_id = script_id
+getgenv().ozas_hub_settings = ozas_hub_settings
 
 local succ, err = pcall(function()
     loadstring(game:HttpGet(("https://api.luarmor.net/files/v3/loaders/%s.lua"):format(script_id)))()
