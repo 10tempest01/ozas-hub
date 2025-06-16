e1eb96ace762867ae50a76b1a560fc9439be45a4
Tempest <59133811+10tempest01@users.noreply.github.com>
2025-06-16 20:52:35 +0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,4 +1,4 @@
-if not script_key and not script_id and not settings then return game.Players.LocalPlayer:Kick("Failed to load. You might be using an old loader, please get the new one in the #script channel.") end
+if not script_key and not script_id and not settings then return warn("something is missing buddy") end
 
 ozas_hub_settings = getgenv().ozas_hub_settings
 
@@ -10,6 +10,6 @@ if err then
     warn(("Ozas Hub failed to load.\n Error:%s"):format(tostring(err)))
 end
 
-local qot = queue_on_teleport and queue_on_teleport or function(...) return (...) end
+local qot = (queue_on_teleport and queue_on_teleport) or (queueonteleport and queueonteleport) (function(...) return (...) end)
 
 qot([[loadstring(game:HttpGet("https://raw.githubusercontent.com/10tempest01/ozas-hub/refs/heads/main/Loader.lua"))()]])
