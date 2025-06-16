54e1e01f686046e8339336f7ec26e9dbe8581f8b
Tempest <59133811+10tempest01@users.noreply.github.com>
2025-06-16 22:21:30 +0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,26 +1,29 @@
-if not script_key and not script_id and not settings then return game.Players.LocalPlayer:Kick("Failed to load. You might be using an old loader, please get the new one in the #script channel.") end
-
-getgenv().ozas_hub_settings = ozas_hub_settings
-getgenv().script_key = script_key
-getgenv().script_id = script_id
-
-local succ, err = pcall(function()
-    loadstring(game:HttpGet(("https://api.luarmor.net/files/v3/loaders/%s.lua"):format(script_id)))()
-end)
-
-if err then
-    warn(("Ozas Hub failed to load.\n Error:%s"):format(tostring(err)))
+if not script_key then
+    return game.Players.LocalPlayer:Kick("Failed to load, script_key not found")
 end
+
+local supported = {
+    [3808081382] = "adcc551b780178b0b9a84b76f9c0ac72" --> TSB
+}
+
+if not supported[game.GameId] then
+    return warn("This game is not supported by Ozas Hub.")
+end
+
+script_id = supported[game.GameId]
+
+task.spawn(function()
+    local succ, err = pcall(function()
+        loadstring(game:HttpGet(("https://api.luarmor.net/files/v3/loaders/%s.lua"):format(script_id)))()
+    end)
+
+    if err then
+        warn(("Failed to load.\n Error:%s"):format(tostring(err)))
+    end
+end)
 
 local qot = (queue_on_teleport and queue_on_teleport) or (queueonteleport and queueonteleport) or (function(...) return (...) end)
 
-qot([[
-    print("testttt")
-    repeat task.wait() until game:IsLoaded()
-
-    ozas_hub_settings=ozas_hub_settings
-    script_key=script_key
-    script_id=script_id
-
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/10tempest01/ozas-hub/refs/heads/main/Loader.lua"))()
-]])
+if execute_on_teleport then
+    queue_on_teleport(([[repeat task.wait() until game:IsLoaded();getgenv().execute_on_teleport=%s;script_key='%s';loadstring(readfile("Loader.Lua"))()]]):format((execute_on_teleport and "true") or "false", script_key))
+end
